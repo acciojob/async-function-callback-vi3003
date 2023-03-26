f69c18c9787f6652e5d4f1ab4f941e6777601cdd
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-26 23:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
+async function fetchData(callback) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+  callback(data);
+}
+
+function displayMessage(data) {
+	document.getElementById("output").innerText = data.title;
+}
 //your JS code here. If required.
